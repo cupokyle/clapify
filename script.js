d2c78ce8bf079b21d9b489d5f050fece6a051bd0
Kyle Forsyth <kyleforsythcomedy@gmail.com>
2021-08-14 00:24:13 -0400

    basic functionality implemented

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 $(document).ready(function(){
   $("#pop-message").hide();
 
+  function clapWords(string) {
+    const splitString = string.split(" ");
+    let solution = [];
+    splitString.forEach((word) => {
+      solution.push(word);
+      solution.push('👏')
+    })
+    return solution.join(" ");
+  }
+  $("#clapify").on("click", () => {
+    let myString = $(".user-input").val();
+    $("#myOutput").val((clapWords(myString)));
+  })
+
+  $("#copyButton").on("click", () => {
+        /* Get the text field */
+        var copyText = document.getElementById("myOutput");
+  
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+      
+        /* Alert the copied text */
+        $("#pop-message").fadeIn( 400 ).delay( 800 ).fadeOut( 400 );
+      
+      }
+  )
 })
 
-function copyText() {
-  /* Get the text field */
-  var copyText = document.getElementById("myOutput");
 
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
 
-  /* Alert the copied text */
-  $("#pop-message").fadeIn( 400 ).delay( 800 ).fadeOut( 400 );
-
-}
